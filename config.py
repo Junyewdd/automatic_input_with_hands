abe96c1827c88700132017fda4b7b81b97c43e70
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-06-04 01:21:23 +0900

    feat: 문장 생성

---
+++ config.py
@@ -0,0 +1,16 @@
+# Define the number of hands and gestures
+max_num_hands = 2
+
+gesture = {
+    0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5',
+    6: '6', 7: '7', 8: '8', 9: '9', 10: '10',
+}
+
+# Define the gesture to alphabet mapping
+alphabet_gesture = {
+    1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F',
+    7: 'G', 8: 'H', 9: 'I', 10: 'J', 11: 'K', 12: 'L',
+    13: 'M', 14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R',
+    19: 'S', 20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X',
+    25: 'Y', 26: 'Z'
+}
--- main.py
+++ main.py
@@ -1,23 +1,8 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-
-# Define the number of hands and gestures
-max_num_hands = 2
-
-gesture = {
-    0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5',
-    6: '6', 7: '7', 8: '8', 9: '9', 10: '10',
-}
-
-# Define the gesture to alphabet mapping
-alphabet_gesture = {
-    1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F',
-    7: 'G', 8: 'H', 9: 'I', 10: 'J', 11: 'K', 12: 'L',
-    13: 'M', 14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R',
-    19: 'S', 20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X',
-    25: 'Y', 26: 'Z'
-}
+from config import max_num_hands, alphabet_gesture
+from gesture_recognition import load_gesture_model, recognize_gesture
 
 # Initialize MediaPipe hands model
 mp_hands = mp.solutions.hands
@@ -29,11 +14,7 @@ hands = mp_hands.Hands(
 )
 
 # Load gesture recognition model
-file = np.genfromtxt('data/gesture_train.csv', delimiter=',')
-angle = file[:, :-1].astype(np.float32)
-label = file[:, -1].astype(np.float32)
-knn = cv2.ml.KNearest_create()
-knn.train(angle, cv2.ml.ROW_SAMPLE, label)
+knn = load_gesture_model()
 
 # Initialize sentence storage
 sentence = ""
@@ -63,24 +44,9 @@ while cap.isOpened():
             for j, lm in enumerate(res.landmark):
                 joint[j] = [lm.x, lm.y, lm.z]
 
-            # Compute angles between joints
-            v1 = joint[[0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15, 0, 17, 18, 19], :]
-            v2 = joint[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], :]
-            v = v2 - v1
-            v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
-
-            angle = np.arccos(np.einsum('nt,nt->n',
-                                        v[[0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14, 16, 17, 18], :],
-                                        v[[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19], :]))
-            angle = np.degrees(angle)
-
-            # Inference gesture
-            data = np.array([angle], dtype=np.float32)
-            ret, results, neighbours, dist = knn.findNearest(data, 3)
-            idx = int(results[0][0])
-
-            if idx in gesture.keys():
-                hand_results.append(gesture[idx])
+            gesture_result = recognize_gesture(knn, joint)
+            if gesture_result:
+                hand_results.append(gesture_result)
 
             mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
 
